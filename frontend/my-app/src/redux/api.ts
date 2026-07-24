837bb3f938d5e52a2a607e91c56df6b702ca74c2
Akshay-KumarS <Akshay.Kumar@sourceadvisors.com>
2026-07-24 17:02:43 +0530

    Made refinements in the api.ts file

--- frontend/my-app/src/redux/api.ts
+++ frontend/my-app/src/redux/api.ts
@@ -1,5 +1,5 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { API_VERSION } from "@/config/version";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:8000/api/v1",
